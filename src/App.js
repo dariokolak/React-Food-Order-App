11615bb90874935e9b1c1f06559b1abcf84a2b6c
Dario Kolak <kolak.dario@hotmail.com>
2022-06-27 19:46:03 +0200

    added all files for the react food order app

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Meals from "./components/Meals/Meals";
+import Header from "./components/Layout/Header";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvaider";
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler}></Header>
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
